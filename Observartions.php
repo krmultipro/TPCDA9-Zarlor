79766edc7df5d63ac67928333810b99786775f20
Kery RICHARD DASSOT <krichard.cda@FORMATION.LOCAL>
2025-10-24 09:57:02 +0400

    creation attributs de la classe

--- Observartions.php
+++ Observartions.php
@@ -2,5 +2,19 @@
 
 class Observartions
 {
+    private $id;
 
+    private $nom_commun;
+
+    private $nom_scientifique;
+
+    private $nb_individus;
+
+    private $zone_observation;
+
+    private $milieu_naturel;
+
+    private $nom_observateur;
+
+    private $date_observation;
 }
