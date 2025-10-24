3057b348b851daf59965f31baaf7d2b4762272d7
Kery RICHARD DASSOT <krichard.cda@FORMATION.LOCAL>
2025-10-24 14:12:25 +0400

    ajout de datatable

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdn.datatables.net/2.3.4/css/dataTables.dataTables.css" />
+    <link rel="stylesheet" href="https://cdn.datatables.net/2.3.4/css/dataTables.dataTables.min.css">
     <link rel="stylesheet" href="style.css">
 
 </head>
@@ -178,10 +178,16 @@
         </table>
     </div>
 </div>
-
 <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
 <script src="https://cdn.datatables.net/2.3.4/js/dataTables.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+<script>
+    let table = new DataTable('#myTable', {
+        language: {
+            url: '//cdn.datatables.net/plug-ins/2.3.4/i18n/fr-FR.json',
+        },
+    });
+</script>
 
 </body>
 </html>
